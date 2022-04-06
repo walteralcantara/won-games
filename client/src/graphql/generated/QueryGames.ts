785da925204b596338b6e209172ccd7d602d2654
Walter Alcantara <waltermalcantara@gmail.com>
2022-04-05 21:53:23 -0300

    Refactor to use mappers

--- client/src/graphql/generated/QueryGames.ts
+++ client/src/graphql/generated/QueryGames.ts
@@ -7,8 +7,29 @@
 // GraphQL query operation: QueryGames
 // ====================================================
 
-export interface QueryGames {}
+export interface QueryGames_games_cover {
+  __typename: 'UploadFile'
+  url: string
+}
+
+export interface QueryGames_games_developers {
+  __typename: 'Developer'
+  name: string
+}
+
+export interface QueryGames_games {
+  __typename: 'Game'
+  name: string
+  slug: string
+  cover: QueryGames_games_cover | null
+  developers: QueryGames_games_developers[]
+  price: number
+}
+
+export interface QueryGames {
+  games: QueryGames_games[]
+}
 
 export interface QueryGamesVariables {
-  limit: number;
+  limit: number
 }
