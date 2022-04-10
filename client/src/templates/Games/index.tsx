2c823841b45fe28d313a125a56ff7641f13e6415
Walter Alcantara <waltermalcantara@gmail.com>
2022-04-10 13:19:42 -0300

    Added custom hook useQueryGames

--- client/src/templates/Games/index.tsx
+++ client/src/templates/Games/index.tsx
@@ -1,8 +1,4 @@
-import { useQuery } from '@apollo/client'
-
-import { QUERY_GAMES } from 'graphql/queries/games'
-import { QueryGames, QueryGamesVariables } from 'graphql/generated/QueryGames'
-
+import { useQueryGames } from 'graphql/queries/games'
 import Base from 'templates/Base'
 import { KeyboardArrowDown as ArrowDown } from '@styled-icons/material-outlined/KeyboardArrowDown'
 
@@ -18,10 +14,9 @@ export type GamesTemplateProps = {
 }
 
 const GamesTemplate = ({ filterItems }: GamesTemplateProps) => {
-  const { data, loading, fetchMore } = useQuery<
-    QueryGames,
-    QueryGamesVariables
-  >(QUERY_GAMES, { variables: { limit: 15 } })
+  const { data, loading, fetchMore } = useQueryGames({
+    variables: { limit: 15 }
+  })
 
   const handleFilter = () => {
     return
